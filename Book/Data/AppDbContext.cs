cc6069bf5801cc9bd93cda49267994dee5309db1
xayotbek <hayotbek2525@gmail.com>
2024-06-25 22:28:29 +0500

    Creating Models And Add Migration

---
+++ Book/Data/AppDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Book.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Book.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Books> Books { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string path = "server=(localdb)\\MSSQLLocalDB;" +
+                          "database=BookDb;" +
+                          "Trusted_Connection = true;";
+
+            optionsBuilder.UseSqlServer(path);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            Books books = new Books()
+            {
+                Id = 1,
+                Title = "So'ngi safar",
+                Author = "Abdullmannon Abdullox",
+                PublishedYear = 2023,
+                Photo = @"X:\books\Book\wwwroot\images\songiSahifa.jpg"
+            };
+
+            modelBuilder.Entity<Books>().HasData(books);
+        }
+
+    }
+}
---
+++ Book/Migrations/20240625172730_FirstMigration.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Book.Migrations
+{
+    /// <inheritdoc />
+    public partial class FirstMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Author = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PublishedYear = table.Column<int>(type: "int", nullable: false),
+                    Photo = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Books",
+                columns: new[] { "Id", "Author", "Photo", "PublishedYear", "Title" },
+                values: new object[] { 1, "Abdullmannon Abdullox", "X:\\books\\Book\\wwwroot\\images\\songiSahifa.jpg", 2023, "So'ngi safar" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+        }
+    }
+}
